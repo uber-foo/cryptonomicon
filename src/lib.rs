8daf0af37d793c94b7276d298cff86d4a8e5dc06
Sean Bruton <sean@uberfoo.net>
2018-10-03 09:50:44 -0500

    saving progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 #![deny(
-    missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts,
-    trivial_numeric_casts, unsafe_code, unstable_features, unused_import_braces,
-    unused_qualifications, unused_variables, unreachable_code, unused_comparisons, unused_imports,
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+    unused_variables,
+    unreachable_code,
+    unused_comparisons,
+    unused_imports,
     unused_must_use
 )]
 
 //! This crate provides a suite of cryptographic services. It brings together numerious other quality crypto crates in addition to local implementations, unifying them all through a common interface.
 
+extern crate failure;
 #[macro_use]
-extern crate error_chain;
+extern crate failure_derive;
 
-pub mod asymmetric;
-pub mod elliptic_curve;
-pub mod errors;
 pub mod pki;
--- src/pki/mod.rs
+++ src/pki/mod.rs
@@ -1,8 +1,31 @@
 //! Public Key Infrastructure (certificates, et al.)
 
+use failure::Error;
+use std::path::Path;
+
+/// Certificate error
+#[derive(Debug, Fail)]
+pub enum CertificateError {
+    /// Certificate could not be loaded from the specified file
+    #[fail(display = "cannot load certificate from \"{}\"", file_name)]
+    FailedToLoad {
+        /// path to certificate file that could not be loaded
+        file_name: String,
+    },
+}
+
 /// Certificate
 #[cfg(feature = "pki")]
 pub trait Certificate {}
+impl Certificate {
+    /// load a certificate from a file
+    pub fn from_file(path: &Path) -> Result<Box<Certificate>, Error> {
+        Err(CertificateError::FailedToLoad {
+            file_name: path.display().to_string(),
+        }
+        .into())
+    }
+}
 
 /// Certificate Request
 #[cfg(feature = "pki")]
